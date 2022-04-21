fab56e3b108b7491ae5b33b202e1351da531c238
Damjan15 <stamenovdamjan20@gmail.com>
2022-04-21 12:18:58 -0700

    Results component created

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,7 +1,43 @@
+import avatar from "../../assets/avatar.png";
+import styles from "./results.module.css";
+
 const Results = () => {
   return (
-    <div>Results</div>
-  )
-}
+    <main className={styles.results}>
+      <div className={styles.resultsHeader}>
+        <div>
+          <img src={avatar} alt="Avatar" />
+        </div>
 
-export default Results
+        <div className={styles.resultsInfo}>
+          <h1>The Octocat</h1>
+          <p className={styles.username}>@octocat</p>
+          <p className={styles.joinedAt}>Joined 25 Jan 2021</p>
+        </div>
+      </div>
+
+      <div className={styles.resultsDescription}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus porro optio blanditiis quaerat et corporis? Ex hic impedit aliquid et veritatis libero molestias labore porro. At laudantium voluptatum quae impedit.</p>
+      </div>
+
+      <div className={styles.resultsTotal}>
+        <div className={styles.result}>
+          <h4>Repos</h4>
+          <h2>8</h2>
+        </div>
+
+        <div className={styles.result}>
+          <h4>Followers</h4>
+          <h2>3838</h2>
+        </div>
+
+        <div className={styles.result}>
+          <h4>Following</h4>
+          <h2>9</h2>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Results;
